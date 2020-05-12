be2b931ab72f457158206a47d6c71483830f88a2
BeauvR <beauvrmc140@gmail.com>
2020-05-12 15:46:54 +0200

    Uitwerking

--- value.php
+++ value.php
@@ -1,1 +1,23 @@
+<?php
+    function addItemToArray($array, $item){
+        $array[] = $item;
+        return $array;
+    }
 
+    function addItemToArray2(&$array, $item){
+        $array[] = $item;
+    }
+
+    $oldArray = [
+        'value1',
+        'value2'
+    ];
+
+    $newArray = addItemToArray($oldArray, 'item');
+
+    addItemToArray2($oldArray, 'item2');
+
+    var_dump($newArray);
+
+    var_dump($oldArray);
+?>
